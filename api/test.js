33b630ed001659d12132ac3b94d06cccef9ed742
mgolia6 <mgolia6@gmail.com>
2026-05-25 21:40:16 -0400

    add env test endpoint

---
+++ api/test.js
@@ -0,0 +1,10 @@
+export default async function handler(req, res) {
+  const vars = {
+    ANTHROPIC_API_KEY:  !!process.env.ANTHROPIC_API_KEY,
+    SUPABASE_URL:       !!process.env.SUPABASE_URL,
+    SUPABASE_KEY:       !!process.env.SUPABASE_KEY,
+    CRON_SECRET:        !!process.env.CRON_SECRET,
+    RESEND_API_KEY:     !!process.env.RESEND_API_KEY,
+  };
+  res.status(200).json({ env: vars, node: process.version });
+}
